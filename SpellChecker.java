e98abd239896fbb339ee7be1ad34993e081b3148
ajrulesu <automac007@gmail.com>
2019-04-09 12:53:12 -0600

    class from textbook

---
+++ SpellChecker.java
@@ -0,0 +1,25 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.SET;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class SpellChecker {  
+    public static void main(String[] args) {
+        SET<String> dictionary = new SET<String>();
+
+        // read in dictionary of words
+        In dict = new In(args[0]);
+        while (!dict.isEmpty()) {
+            String word = dict.readString();
+            dictionary.add(word);
+        }
+        StdOut.println("Done reading dictionary");
+
+        // read strings from standard input and print out if not in dictionary
+        StdOut.println("Enter words, and I'll print out the misspelled ones");
+        while (!StdIn.isEmpty()) {
+            String word = StdIn.readString();
+            if (!dictionary.contains(word)) StdOut.println(word);
+        }
+    }
+}
